c68e73211ecfe8485bc9f31f38b5722d561d0817
scorpioneater <63227198+scorpioneater@users.noreply.github.com>
2025-02-04 11:56:31 -0700

    Update hashp.pm

--- hashp/hashp.pm
+++ hashp/hashp.pm
@@ -1,9 +1,15 @@
 #hashp, the hash processor and hash|.
 
 while (<STDIN>){  push(@lines,$_);}
-@tokens;@funcSymbols = ('+','-','*','.+','&');sub addition {  my ($x,$y) = @_;  return ($x+$y);}sub subtraction {  my ($x,$y) = @_;  return ($x-$y);}sub multiplication {  my ($x,$y) = @_;  return ($x*$y);}sub concatenation {  my ($x,$y) = @_;  return (${x}.${y});}
+@tokens;@funcSymbols = ('+','-','*','.+','&');
+sub addition {  my ($x,$y) = @_;  return ($x+$y);}
+sub subtraction {  my ($x,$y) = @_;  return ($x-$y);}
+sub multiplication {  my ($x,$y) = @_;  return ($x*$y);}
+sub concatenation {  my ($x,$y) = @_;  return (${x}.${y});}
 %customFunctions = ();
-sub newfunc {  my ($symbol) = shift;  print "Creating function for ${symbol}\n";  push(@funcSymbols,$symbol);  $customFunctions{$symbol} = @_;}
+sub newfunc {  my ($symbol) = shift;
+print "Creating function for ${symbol}\n";
+push(@funcSymbols,$symbol);  $customFunctions{$symbol} = @_;}
 
 %functions = (  '+' => \&addition,  '-' => \&subtraction,  '*' => \&multiplication,  '.+' => \&concatenation,  '&' => \&newfunc);
 my $sum = &{$functions{'+'}}(2,2);print "${sum}\n";
@@ -12,7 +18,8 @@ sub Container::new {  my $class = shift;   my $self = {      _name => shif
 my $c = new Container("yyz","-");my @a = (23,75,97);$c->addArgsToBody(0,\@a);print "\n";print $c->toString();print "\n";
 %containers = ();
 $state = "INIT";
-map {  map {body($_)} split (/ /,$_);} @lines;
+map {  map {body($_)} split (/ /,$_);}
+@lines;
 sub body() {  print "Current Token:${_} State:${state}\n";  if ($state eq "INIT"){    if($_ eq '{'){       print "Found function def opening\n";    } else {      if ($_ =~ /^[a-zA-Z]+\w+$/){         print "Found container name\n";         push (@tokens,$_);         $state = "BEGIN_CONTAINER";      } else {        if($_ =~ /^\w+\(.*\)/){           print "Found container element\n";           $state = "BEGIN_CONTAINER_ELEMENT";           parseContainerElement($_);           $state = "INIT";        }      }    }  }elsif ($state eq "BEGIN_CONTAINER") {    if ($_ ~~ @funcSymbols) {      print "Found defined function symbol\n";      push (@tokens,$_);      $state = "FOUND_SYMBOL";    } else {      print "PARSE ERROR: expected function symbol, \"${_}\" is undefined\n";      exit 1;    }  }elsif ($state eq "FOUND_SYMBOL") {    if ($_ =~ /^\[/) {      if ($_ !~ /\]/) {      print "Found container opening bracket\n";      $state = "FOUND_CONTAINER_START";      push (@containerTokens,$_);      }    }  } elsif ($state eq "FOUND_CONTAINER_START") {    if ($_ !~ m/\]/) {      print "Ending bracket not found, pushing on stack\n";      push (@containerTokens,$_);    } else {      print "Found container closing bracket\n";      if ($_ !~ m/\]$/) {        print "PARSE_ERROR: Symbols found after closing bracket, space expected\n";        exit 1;      }            $state = "FOUND_CONTAINER_END";      push (@containerTokens,$_);      if(parseContainer()){        #$state = "CREATED_CONTAINER";        $state = "INIT";      } else {        print "PARSE_ERROR: Error creating container/n";        exit 1;      }    }  } elsif ($state eq "CREATED_CONTAINER"){    if($_ =~ /^\w+\(.*\)/){      print "Found container element\n";      parseContainerElement($_);      $state= "INIT";    }  } elsif ($state eq "BEGIN_CONTAINER_ELEMENT"){      }}
 sub parseContainer {  my $containerBody = join (' ', @containerTokens);  print "Parsing container body ${containerBody}\n";  #$containerBody =~ s/^.*\(//;  #$containerBody =~ s/\).*//;  $containerBody =~ s/\[//;  $containerBody =~ s/\]//;  print $containerBody."\n";  #chomp($containerBody);  #$containerBody = removeBrackets($containerBody);  #     substr ($containerBody,1,length($containerBody)-2);   #print $containerBody."\n";    my @containerElements = split (/,/,$containerBody);  print @tokens;  print "\n";    my $symbol = pop @tokens;  my $name = pop @tokens;  print "name:${name} symbol:${symbol}\n";    my $c = new Container($name,$symbol);
   my $i=0;  map {parseElement($_,$i++,$c)} @containerElements;  $containers{$name} = $c;  print $containers{$name}->toString();  print "\n";  @containerTokens = ();  return true;}
