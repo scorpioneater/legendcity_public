24511401a036b7e48b289f48c2a8f575a80b75e5
scorpioneater <63227198+scorpioneater@users.noreply.github.com>
2025-12-15 06:29:51 -0700

    Update Biff.java

--- ace_biffstein/biff/Biff.java
+++ ace_biffstein/biff/Biff.java
@@ -1,1 +1,33 @@
-a
+package com.msc.biff
+
+public class Biff {
+
+     public static void main (Sting [] args) 
+            
+     }
+
+     private static void getNFL() {
+       
+     }
+
+     private static void getNHL() {
+       
+     }
+
+     private static void getNBA() {
+       
+     }
+
+     private static void getCFB() {
+       
+     }
+
+     private static void getCBB() {
+       
+     }
+
+     private static void getIFB) {
+       
+     }
+  
+}
