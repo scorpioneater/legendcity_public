b2d509dfbf3566df85f877c524c6a74971db1bc8
scorpioneater <63227198+scorpioneater@users.noreply.github.com>
2025-12-15 06:37:16 -0700

    Update Biff.java

--- ace_biffstein/biff/Biff.java
+++ ace_biffstein/biff/Biff.java
@@ -7,7 +7,12 @@ public class Biff {
      }
 
      private static void getNFL() {
-       
+          
+     }
+
+     private float getFbPScore() {
+          float ln = 
+           return 1/(1 + math.pow(math.e,-1*ln))  
      }
 
      private static void getNHL() {
@@ -26,8 +31,39 @@ public class Biff {
        
      }
 
-     private static void getIFB) {
+     private static void getIFB() {
        
-     }
-  
+     }  
 }
+
+/*
+p() calculation: 
+def p(stats):
+    print (stats)
+    def_1st_down  = stats[0]#7
+    def_pass_yds  = stats[1]#24
+    def_total_yds = stats[2]#24 + 39
+    def_turnovers = stats[3]#33 + 76
+    home          = stats[4]#NA
+    losses        = stats[5]#NA
+    off_1st_downs = stats[6]#6
+    off_rush_yds  = stats[7]#38
+    off_total_yds = stats[8]#48
+    off_turnovers = stats[9]#32 + 75
+    wins          = stats[10]#NA
+     
+    ln = 0.0488                        \
+         - 0.0428*def_1st_down   \
+         + 0.00651*def_pass_yds  \
+         - 0.0145*def_total_yds   \
+         + 0.9124*def_turnovers  \
+         + 0.5319*home           \
+         - 0.0827*losses         \
+         + 0.0331*off_1st_downs  \
+         + 0.00794*off_rush_yds  \
+         + 0.00806*off_total_yds \
+         - 0.9686*off_turnovers  \
+         + 0.0845*wins   
+
+    return 
+*/
