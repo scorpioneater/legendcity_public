08d29b4c91b41583ae0d31809874c76439988e8f
scorpioneater <63227198+scorpioneater@users.noreply.github.com>
2026-01-07 05:28:18 -0700

    Removed python sample code
    Removed unused p() calculation and related comments.

--- ace_biffstein/biff/Lombardi.java
+++ ace_biffstein/biff/Lombardi.java
@@ -4,7 +4,6 @@ package com.msc.biff;
    Startegy: 
             1. Use p() value to find games that are expected to be lopsided and games that are a "coin toss"
             2. 
-
 */
 public class Lombardi {
 
@@ -35,35 +34,3 @@ public class Lombardi {
           
      }
   }
-
-/*
-p() calculation: 
-def p(stats):
-    print (stats)
-    def_1st_down  = stats[0]#7
-    def_pass_yds  = stats[1]#24
-    def_total_yds = stats[2]#24 + 39
-    def_turnovers = stats[3]#33 + 76
-    home          = stats[4]#NA
-    losses        = stats[5]#NA
-    off_1st_downs = stats[6]#6
-    off_rush_yds  = stats[7]#38
-    off_total_yds = stats[8]#48
-    off_turnovers = stats[9]#32 + 75
-    wins          = stats[10]#NA
-     
-    ln = 0.0488                        \
-         - 0.0428*def_1st_down   \
-         + 0.00651*def_pass_yds  \
-         - 0.0145*def_total_yds   \
-         + 0.9124*def_turnovers  \
-         + 0.5319*home           \
-         - 0.0827*losses         \
-         + 0.0331*off_1st_downs  \
-         + 0.00794*off_rush_yds  \
-         + 0.00806*off_total_yds \
-         - 0.9686*off_turnovers  \
-         + 0.0845*wins   
-
-    return 
-*/
